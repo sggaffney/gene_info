59c1b8d8e8054b53f43744f69a36a758798a901d
Stephen Gaffney <stephen.gaffney@yale.edu>
2016-06-28 13:42:41 +0100

    Update requirements and package_data in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-__author__ = 'Stephen G. Gaffney'
-
 from setuptools import setup
+
+__author__ = 'Stephen G. Gaffney'
 
 setup(name='gene_info',
       version='0.1',
-      description='Fetch canonical transcript info and generate S/NS bed files.',
+      description='Fetch canonical transcript info and generate S/NS bed '
+                  'files.',
       url='http://github.com/sggaffney',
       author='Stephen G. Gaffney',
       author_email='stephen.gaffney@yale.edu',
       license='GPLv3',
       packages=['gene_info'],
+      package_data={'gene_info': ['data/*', 'config/*.ini']},
       install_requires=[
-            'numpy', 'sqlalchemy', 'pyfaidx', 'requests', 'pandas',
-            'configparser'
+          'numpy', 'pyfaidx', 'requests', 'pandas', 'configparser', 'biopython'
       ],
       scripts=['bin/build_gene_beds', 'bin/lookup_hg19'],
-      zip_safe=True,
-      # data_files=[('gene_matcher', ['gene_matcher/gene_lookup_refs.db'])]
+      zip_safe=False,
       )
